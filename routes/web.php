0db68f21223f9167e4cfa1dd5b9b996ac4fc3427
Jeanmaiafernandes <jeanmaiafernandes@gmail.com>
2022-04-22 15:11:53 -0300

    criando menu de navegação

--- routes/web.php
+++ routes/web.php
@@ -28,19 +28,9 @@ Route::get('/', [PrincipalController::class, 'principal']);
 Route::get('/sobre-nos', [SobreNosController::class, 'SobreNos']);
 
 Route::get('/contato', [ContatoController::class, 'Contato']);
-// nome, categoria, assunto, mensagem
 
- 
-Route::get(
-    '/contato/{nome}/{categoria_id}', 
- function (
-  string $nome = "Desconhecido",
-  int $categoria_id = 1 // 1 - 'Informação'
-  ) {
-    echo "Estamos aqui: $nome - $categoria_id";
-    }
-)->where('categoria_id', '[0-9]+')
--> where('nome', '[A-Za-z]+');
+
+
 
 
 /* verbo http
