1ba96c96cfa327c5ba34a8a2ea877cba510a508e
Jeanmaiafernandes <jeanmaiafernandes@gmail.com>
2022-04-25 21:54:04 -0300

    Incluíndo comentários e blocos PHP puros

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,18 @@
+<h3>Fornecedor</h3>
+
+{{ 'texto de teste' }}
+<?= 'texto de teste'?>
+
+
+{{-- Fica o comentário que será descartado pelo interpretador do blade --}}
+
+@php
+
+    // Para comentários de uma linha
+    /*
+     * Comentários de multiplas linhas
+    */
+
+    echo 'Texto de teste'
+
+@endphp
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use  Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,48 +21,37 @@ Route::get('/', function () {
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\TesteController;
 
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/', [PrincipalController::class, 'principal'])->name("site.index");
-
-Route::get('/sobre-nos', [SobreNosController::class, 'SobreNos'])->name("site.sobrenos");
+Route::get('/sobre-nos', [SobreNosController::class, 'SobreNos'])->name('site.sobrenos');
 
 Route::get('/contato', [ContatoController::class, 'Contato'])->name("site.contato");
 
-Route::get('/login', function(){  return "Login"; })->name("site.login");
+Route::get('/login', function(){  return 'Login'; })->name('site.login');
 
 
-  
+
   Route::prefix('/app')->group(function() {
 
-    Route::get('/clientes', function() { return "Clientes"; })->name("app.clientes");
-    Route::get('/fornecedores', function() { return "Fornecedores"; })->name("app.fornecedores");
-    Route::get('/produtos', function() { return "Produtos"; })->name("app.produtos");
-    
+    Route::get('/clientes', function() { return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos', function() { return 'Produtos'; })->name('app.produtos');
+
   });
 
 
-Route::get('/rota1', function() {
-    echo "Rota 1";
+Route::get('/teste/{p1}/{p2}', [\App\Http\Controllers\TesteController::class, 'teste'])->name('teste');
 
-})->name('site.rota1');
-
-
-
-Route::get('/rota2', function() {
-    return redirect()->route('site.rota1');
-
-})->name('site.rota2');
-
-//Route::redirect('/rota2', '/rota1');
 
 Route::fallback(function (){
-    echo "A rota acessada não existe. <a href=".route("site.index").">clique aqui</a> para ir para página inicial";
+    echo 'A rota acessada não existe. <a href= .route (" site.index ")."<a/>clique aqui</a> para ir para página inicial';
 
 });
 
 
-/* verbo http
+/* Verb http
 
 get
 post
